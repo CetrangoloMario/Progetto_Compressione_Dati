31e34fdde6290722e02bf1e64c11f6ef68768709
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2021-12-18 09:38:30 +0100

    testing

---
+++ HologramCompression.py
@@ -0,0 +1,159 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy.io
+import math as math
+import scipy.fft
+from scipy.linalg import dft
+from scipy.fftpack import fft, ifft
+import confresnel1D
+import fourierfresnel1D
+
+
+def intfresnel2D(x, fw, pp, z, wlen, algo):
+    # aggiungere un assert per controllare se l'array x non contiene zero
+    assert (np.size(x) % 2) == 0, "Hologram dimensions must be even"
+    assert np.size(x, 0) == np.size(x, 1), "Hologram must be square (to be resolved in next version)"
+
+    def fw_fresnel(r):
+        return np.round(fpropfun(r, pp, fz, wlen))
+
+    def bw_fresnel(r):
+        return np.round(bpropfun(r, pp, bz, wlen))
+
+    def fwdfourierfresnel1D(x, pp, z, wlen):
+        return fourierfresnel1D.fourierfresnel1D(x, pp, z, wlen, True)
+
+    def revfourierfresnel1D(x, pp, z, wlen):
+        return fourierfresnel1D.fourierfresnel1D(x, pp, z, wlen, False)
+
+    def apply_transform():
+        if (fw):
+            o = np.conjugate(x[:, 1::2])
+            e = x[:, 0::2]
+
+            e = e + bw_fresnel(o)
+            o = o - fw_fresnel(e)
+            e = e + bw_fresnel(o)
+
+            x[:, 0::2] = -o
+            x[:, 1::2] = np.conjugate(e)
+
+            return x
+        else:
+            e = np.conj(x[:, 1::2])  # righe le prende tutte, colonne prende a 2 a 2
+            o = -x[:, 0::2]  # righe le prende tutte, colonne parte dalla prima  e a step di 2
+
+            e = e - bw_fresnel(o)
+            o = o + fw_fresnel(e)
+            e = e - bw_fresnel(o)
+
+            x[:, 0::2] = e
+            x[:, 1::2] = np.conj(o)
+            return x
+
+    if algo == "conv":
+        fpropfun = confresnel1D.convfresnel1D  # convfresnel1D(x,pp,z,wlen)
+        bpropfun = confresnel1D.convfresnel1D  # convfresnel1D(x,pp,z,wlen)
+        fz = z
+        bz = -z
+    elif algo == "four":
+        fpropfun = fwdfourierfresnel1D
+        fz = z
+        bpropfun = revfourierfresnel1D
+        bz = z
+    else:
+        print("algoritmo sconosciuto")
+
+    for i in [-1, 1]:
+        if fw:
+            x = np.rot90(x, i)
+        x = apply_transform()
+        if not fw:
+            x = np.rot90(x, i)
+    return x
+
+
+def givenphaserot(x, p, fw):
+    assert np.size(x) == np.size(p), "Input argument X & P must be equal dimension"
+
+    # givens rotation
+    def givens_rot(theta, x):
+        m = np.abs(theta) < peps
+        m = m.astype(int) #ok m
+        a = np.divide((np.cos(theta) - 1), np.sin(theta))
+        a[m] = 0
+        b = np.sin(theta)
+        b[m] = 0
+        if fw:
+            sg = 1
+        else:
+            sg = -1
+
+        x = x + sg * np.round(np.multiply(a, np.imag(x)))
+        #print(x[542,521])
+        x = x + sg * 1j * np.round(np.multiply(b, np.real(x)))
+        #print(x[542,521])
+        x = x + sg * np.round(np.multiply(a, np.imag(x)))
+        #print(x[542,521])
+        return x
+
+    # determine in what phase quadrant  to operate
+    def quadrant_swap(q):
+        m = np.logical_or(q == 1, q == 3)
+        m = m.astype(int)  # converto la matrice da boolean a int
+        x[m] = -np.imag(x[m]) + 1j * np.real(x[m])
+        #print("X: ",x[m])
+        x[q > 1] = -x[q > 1]
+        return x
+
+    q = np.mod(np.floor((2 * (p/np.pi)) + .5), 4)
+    print(q[4,4])
+    somma = np.pi/2
+    t = np.mod(p+(np.pi/4),np.pi/2) - (np.pi/4)
+    #print(t[4,4]) #0.7291
+    peps = 1e-7  # phase epsilon
+    if fw:
+        x = quadrant_swap(q)
+    x = givens_rot(t, x)
+    if not fw:
+        """x = quadrant_swap(np.mod(4 - q, 4))"""
+    return x
+
+
+def fourier_phase_multiply(r, fw, pp, z, wlen):
+    n = np.size(r, 0)
+    xx = np.power(np.matrix(np.arange(-n / 2, n / 2)), 2)
+    ppout = wlen * np.abs(z) / n / pp
+    p = math.pi * np.power(ppout, 2) / np.multiply((wlen * np.abs(z)), (xx + xx.transpose())) + 2 * np.pi * z / wlen
+    return givenphaserot(r,p, fw)
+
+
+def main():
+    f = scipy.io.loadmat('exampleholo.mat')  # aprire il file .mat
+    pp = 8e-6  # pixel pitch
+    pp = np.matrix(pp)
+    wlen = 532e-9  # wavelenght
+    wlen = np.matrix(wlen)
+    dist = 5e-1  # propogation depth
+    dist = np.matrix(dist)
+
+    holo = np.matrix(f.get("holo"))  # estraggo dal file .mat solo holo e lo converto da np a array
+
+    # plt.imshow(np.real(holo), cmap="gray")
+
+    # plt.show()  # mostro l'ologramma pre - compressione
+    # algo = "conv"
+    algo = 'four';
+    t = intfresnel2D(np.csingle(holo), False, pp, dist, wlen, algo)
+    # res = np.real(t)
+    # print("res: ",res[255,254])
+    plt.show()
+    t = fourier_phase_multiply(t, False, pp, dist, wlen)
+    plt.imshow(np.real(t), cmap='gray')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+    # prova()
